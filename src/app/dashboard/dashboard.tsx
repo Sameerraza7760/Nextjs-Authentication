ab497a2bef6e3f858fdf71eba588fa972eb5be5b
Sameerraza7760 <sameerraza7760@gmail.com>
2024-10-24 08:06:51 -0700

    authentication in good way in nextjs 15

---
+++ src/app/dashboard/dashboard.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { logout } from "../login/action";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <button onClick={() => logout()}>Logout</button>
+    </div>
+  );
+}
